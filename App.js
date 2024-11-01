dcf237700b4267a8ed20cb6a9b69a5c52e60ce15
ChrisUbario <20151656@aguascalientes.tecnm.mx>
2024-11-01 03:18:38 -0600

    using theme

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from "@react-navigation/native";
-import { NativeBaseProvider } from 'native-base';
+import { Box, NativeBaseProvider, VStack, useColorModeValue } from 'native-base';
+import theme from './theme';
+import ToggleDarkMode from './ToggleDarkMode';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from './src/screens/Home';
 import Reports from './src/screens/Reports';
@@ -43,24 +45,29 @@ const MainTab = () => {
       <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
       <Tab.Screen name="Config" component={Config} options={{ headerShown: false }} />
     </Tab.Navigator>
-  )
+  );
 }
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={isAuthenticated ? "MainTab" : "Login"}>
-          <Stack.Screen name="Login" options={{ headerShown: false }}>
-            {() => <Login setIsAuthenticated={setIsAuthenticated} />}
-          </Stack.Screen>
-          <Stack.Screen name="Register" options={{ headerShown: false }}>
-            {() => <Register setIsAuthenticated={setIsAuthenticated} />}
-          </Stack.Screen>
-          <Stack.Screen name="MainTab" component={MainTab} options={{ headerShown: false }} />
-        </Stack.Navigator>
+        <VStack flex={1} bg={useColorModeValue("light.background.50", "dark.background.900")}>
+          <Box safeAreaTop bg={useColorModeValue('light.background.100', 'dark.background.900')}>
+            <ToggleDarkMode />
+          </Box>
+          <Stack.Navigator initialRouteName={isAuthenticated ? "MainTab" : "Login"}>
+            <Stack.Screen name="Login" options={{ headerShown: false }}>
+              {() => <Login setIsAuthenticated={setIsAuthenticated} />}
+            </Stack.Screen>
+            <Stack.Screen name="Register" options={{ headerShown: false }}>
+              {() => <Register setIsAuthenticated={setIsAuthenticated} />}
+            </Stack.Screen>
+            <Stack.Screen name="MainTab" component={MainTab} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </VStack>
       </NavigationContainer>
     </NativeBaseProvider>
   );
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
-import { NativeBaseProvider, Box, Heading, VStack, FormControl, HStack, Input, Button, Link, Center } from "native-base";
+import { Text, Image } from 'react-native';
+import { NativeBaseProvider, Box, Heading, VStack, FormControl, HStack, Input, Button, Link, Center, useColorModeValue, useBreakpointValue } from "native-base";
 import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = ({ setIsAuthenticated }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigation = useNavigation();
+
+    const bgColor = useColorModeValue('light.background.50', 'dark.background.900');
+    const textColor = useColorModeValue('light.text.50', 'dark.text.50');
+    const linkColor = useColorModeValue("indigo.500", "indigo.300"); 
+
+    const flexDir = useBreakpointValue({ base: 'column', lg: 'row' });
 
     const handleLogin = () => {
         if (email && password) { 
@@ -18,20 +24,21 @@ const LoginScreen = ({ setIsAuthenticated }) => {
     };
 
     return (
-        <Center w="100%">
+        <Center w="100%" bg={bgColor} flex={1}>
+            <Image 
+                source={require('../../assets/icon.png')} 
+                style={{ width: '100%', height: 200, marginBottom: 20 }} 
+                resizeMode="contain"
+            />
             <Box safeArea p="2" py="8" w="90%" maxW="290">
-                <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
-                    color: "warmGray.50"
-                }}>
+                <Heading size="lg" fontWeight="600" color={textColor}>
                     Welcome
                 </Heading>
-                <Heading mt="1" _dark={{
-                    color: "warmGray.200"
-                }} color="coolGray.600" fontWeight="medium" size="xs">
+                <Heading mt="1" color={textColor} fontWeight="medium" size="xs">
                     Sign in to continue!
                 </Heading>
 
-                <VStack space={3} mt="5">
+                <VStack space={3} mt="5" flexDirection={flexDir}>
                     <FormControl>
                         <FormControl.Label>Email</FormControl.Label>
                         <Input value={email} onChangeText={setEmail} />
@@ -42,7 +49,7 @@ const LoginScreen = ({ setIsAuthenticated }) => {
                         <Link _text={{
                             fontSize: "xs",
                             fontWeight: "500",
-                            color: "indigo.500"
+                            color: linkColor 
                         }} alignSelf="flex-end" mt="1">
                             Forget Password?
                         </Link>
@@ -51,9 +58,7 @@ const LoginScreen = ({ setIsAuthenticated }) => {
                         Login
                     </Button>
                     <HStack mt="6" justifyContent="center">
-                        <Text fontSize="sm" color="coolGray.600" _dark={{
-                            color: "warmGray.200"
-                        }}>
+                        <Text fontSize="sm" color={textColor}>
                             I'm a new user. 
                             <Button
                                 variant="link"
